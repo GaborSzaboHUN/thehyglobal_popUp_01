8c570e70739b1cac3ea6ac9e157cbac32ea19c6c
GaborSzaboHUN <66555347+GaborSzaboHUN@users.noreply.github.com>
2024-04-07 23:58:28 +0200

    Buttons click function implemented

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-const adsCloseButton = document.querySelector('.ads-btn.close')
+const adsButtons = document.querySelectorAll('.ads-btn')
 const hysocietyAdsContainer = document.querySelector('.hysociety-ads.auction.active')
+const adsBlur = document.querySelector('.ads-blur.active')
 
 /* - - - - CLOSE ADS CONTAINER - - - - */
 
-adsCloseButton.addEventListener('click', () => {
-    hysocietyAdsContainer.classList.remove('active')
+function adsButtonClick() {
+    hysocietyAdsContainer.style.opacity = 0;
+    adsBlur.style.opacity = 0;
+
+    setTimeout(() => {
+        hysocietyAdsContainer.classList.remove('active')
+        adsBlur.classList.remove('active')
+    }, 300);
+}
+
+adsButtons.forEach((adsButton) => {
+    adsButton.addEventListener('click', adsButtonClick)
 })
